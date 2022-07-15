57394416cf12547b2ad2d59aa7cb1ab86afba137
Edward Brito Diaz <37183622+Edwardb11@users.noreply.github.com>
2022-07-15 13:10:56 -0400

    feat: getState

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -1,14 +1,20 @@
-import React, { useState } from "react";
+import React, { useState,useEffect } from "react";
 import { BrowserRouter as Router, Switch } from "react-router-dom";
 import Home from "../pages/Home";
 import PrivateRouter from "./PrivateRouter";
 
-
 import AuthRouter from "./AuthRouter";
 import PublicRouter from "./PublicRouter";
+import { store } from "../store/store";
 
 const AppRouter = () => {
+  const state = store.getState();
   const [log, setLog] = useState(false);
+console.log(state.payload.login);
+  useEffect(() => {
+      setLog(state.payload.login)
+  }, [state])
+  
   return (
     <Router>
       <Switch>
