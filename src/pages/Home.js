314285921270c8f4c69e1223287b8a2fc6c70067
Edward Brito Diaz <37183622+Edwardb11@users.noreply.github.com>
2022-07-20 14:32:27 -0400

    feat: update landing page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,16 +3,15 @@ import jwt_decode from "jwt-decode";
 import { Link } from "react-router-dom";
 import NavBar from "../components/NavBar";
 import User from "../context/userContext";
+import LandingPage from "../components/LandingPage";
 
 const Home = () => {
   const [token, setToken] = useState("");
-  const [email, setEmail] = useState("");
-  const [name, setName] = useState("");
-  const [userId, setUserId] = useState("");
   const { user, setUser } = useContext(User);
 
-  const refreshToken = async () => {
-    try {
+  useEffect(() => {
+    let isEmpty = JSON.stringify(user) === "{}";
+    if (isEmpty) {
       const token = localStorage.getItem("jwtToken");
       setToken(token);
       const decoded = jwt_decode(token);
@@ -22,25 +21,15 @@ const Home = () => {
         userId: decoded.userId,
         sexo: decoded.sexo,
       });
-    } catch (error) {
-      if (error.response) {
-        console.log(error.response);
-      }
-    }
-  };
-  
-  useEffect(() => {
-    let isEmpty = JSON.stringify(user) === "{}";
-    if (isEmpty) {
-      refreshToken();
     } else {
       return () => {};
     }
-  }, [user, refreshToken]);
+  }, [user,setUser]);
 
   return (
     <div>
       <NavBar />
+      <LandingPage/>
       Bienvenido NOMBRE: {user.name} EMAIL-{user.email} USERID-{user.userId} - sexo -{user.sexo} -
       <Link to="/NoFound">No FOUND</Link>
     </div>
