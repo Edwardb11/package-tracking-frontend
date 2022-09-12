bc376bd30585d7447c884aceea1b05595338cc3e
Edward Brito Diaz <37183622+Edwardb11@users.noreply.github.com>
2022-09-12 13:07:20 -0400

    feat: implements refactor logout and auth

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,7 +4,7 @@ import { Disclosure, Menu, Transition } from "@headlessui/react";
 import { MenuIcon, XIcon } from "@heroicons/react/outline";
 import { Link, NavLink } from "react-router-dom";
 import User from "../context/userContext";
-import setAuthorizationToken from "../utils/setAuthorizationToken";
+import { logout } from "../utils/auth";
 
 const navigation = [
   { name: "Inicio", href: "/", current: true },
@@ -32,12 +32,6 @@ export default function NavBar() {
     }
   }, [setWidthState, width]);
   const { user, setLog, setUser } = useContext(User);
-  const logout = () => {
-    sessionStorage.removeItem("jwtToken");
-    setAuthorizationToken(false);
-    setUser({});
-    setLog(false);
-  };
 
   return (
     <Disclosure as="nav" className="bg-gray-800">
@@ -133,7 +127,7 @@ export default function NavBar() {
                       <Menu.Item>
                         {({ active }) => (
                           <div
-                            onClick={logout}
+                            onClick={() => logout(user, setLog, setUser)}
                             className={classNames(
                               active ? "bg-gray-100" : "",
                               "block px-4 py-2 text-sm text-gray-700 cursor-pointer	 "
