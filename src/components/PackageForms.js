c660686201584e785fc1dc503285a3f046406bd1
Edward Brito Diaz <37183622+Edwardb11@users.noreply.github.com>
2022-09-20 12:37:03 -0400

    feat: layout

--- src/components/PackageForms.js
+++ src/components/PackageForms.js
@@ -1,150 +1,245 @@
-import React, { useState } from "react";
-import UserFinalForms from "./UserFinalForms";
+import React from "react";
 
-function PackageForms() {
-  const [viewForm, setViewForm] = useState(false);
-  const [namePackage, setNamePackage] = useState("");
-  const [amount, setAmount] = useState("");
-  const [weight, setWeight] = useState("");
-  const [type, setType] = useState("");
-  const [userFinal, setUserFinal] = useState("");
-  const handleAdd = () => {
-    setViewForm(!viewForm);
-  };
+const PackageForms = () => {
   return (
-    <>
-      <div className="max-w-2xl mx-auto bg-white p-16">
-        <form>
-          <div className="grid gap-6 mb-6 lg:grid-cols-2">
-            <div>
-              <label
-                htmlFor="nombre"
-                className="block mb-2 text-sm font-semibold px-1 text-gray-900 dark:text-gray-900"
-              >
-                Nombre
-              </label>
-              <input
-                type="text"
-                id="nombre"
-                value={namePackage}
-                onChange={(e) => setNamePackage(e.target.value)}
-                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                placeholder="Ej:Computadora"
-                required
-              />
-            </div>
-            <div>
-              <label
-                htmlFor="tipo"
-                className="block mb-2 text-sm font-semibold px-1 text-gray-900 dark:text-gray-900"
-              >
-                Tipo de paquete
-              </label>
-              <select
-                id="tipo"
-                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                required
-                onChange={(e) => setType(e.target.value)}
-              >
-                <option disabled>Selecione</option>
-                <option value="1" selected>
-                  Alimentación
-                </option>
-                <option value="2">Hogar y electrodoméstico</option>
-                <option value="3">Bebidas</option>
-                <option value="4">Cuidado Personal y Belleza</option>
-                <option value="5">Limpieza</option>
-              </select>
-            </div>
-            <div>
-              <label
-                htmlFor="cantidad"
-                className="block mb-2 text-sm font-semibold px-1 text-gray-900 dark:text-gray-900"
-              >
-                Cantidad (LB)
-              </label>
-              <input
-                type="number"
-                id="cantidad"
-                value={amount}
-                onChange={(e) => setAmount(e.target.value)}
-                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                placeholder="Ej:10"
-                required
-              />
-            </div>
-            <div>
-              <label
-                htmlFor="peso"
-                className="block mb-2 text-sm font-semibold px-1 text-gray-900 dark:text-gray-900"
-              >
-                Peso (LB)
-              </label>
-              <input
-                type="number"
-                name="peso"
-                value={weight}
-                onChange={(e) => setWeight(e.target.value)}
-                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                placeholder="Ej:10"
-                required
-              />
-            </div>
-          </div>
-
-          <div>
-            <p className="text-sm text-justify mb-2 mt-6">
-              Registra un usuario final.
-            </p>
-
-            <button
-              type="button"
-              onClick={handleAdd}
-              className="text-white mb-6 bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
-            >
-              {!viewForm ? "Agregar usuario" : "Cerrar"}
-            </button>
-          </div>
-
-          {!viewForm && (
-            <div>
-              <div>
-                <p className="text-sm text-justify mb-2">
-                  ¿Habías agregado a alguien anteriormente? Seleccionalo{" "}
-                </p>
-                <label
-                  htmlFor="user_final"
-                  className="block mb-2 text-sm font-semibold px-1 text-gray-900 dark:text-gray-900"
-                >
-                  Usuario final
-                </label>
-                <select
-                  id="tipo"
-                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                  required
-                  onChange={(e) => setUserFinal(e.target.value)}
-                >
-                  <option disabled>Selecione</option>
-                  <option value="1" selected>
-                    No tienes personas agregadas
-                  </option>
-                  <option value="2">Una persona</option>
-                </select>
+    <div className="bg-gray-100 mx-auto max-w-6xl bg-white py-20 px-12 lg:px-24 shadow-xl mb-24">
+      <div className="mt-10 sm:mt-0">
+        <div className="md:grid md:grid-cols-1 md:gap-6">
+          <div className="mt-5 md:mt-0 md:col-span-2">
+            <form>
+              <div className="shadow overflow-hidden sm:rounded-md">
+                  <h1 className="text-center font-medium text-medium">Complete el siguiente formulario para agregar un paquete</h1>
+                <div className="px-2 py-2 bg-white sm:p-6">
+                  <div className="grid grid-cols-6 gap-6">
+                    <div className="col-span-6">
+                      <label
+                        htmlFor="street_address"
+                        className="block text-sm font-medium ">
+                        Información del paquete{" "}
+                      </label>
+                    </div>
+                    <div className="col-span-6 sm:col-span-6 lg:col-span-2">
+                      <label
+                        htmlFor="city"
+                        className="block text-sm font-medium text-gray-700">
+                        Nombre*
+                      </label>
+                      <input
+                        type="text"
+                        name="namePackage"
+                        placeholder="Ej: Mesa Gaming"
+                        required
+                        className="w-full bg-gray-200 text-black border border-gray-200 rounded py-3 px-4 mb-3"
+                      />
+                    </div>
+                    <div className="col-span-6 sm:col-span-3 lg:col-span-2">
+                      <label
+                        htmlFor="state"
+                        className="block text-sm font-medium text-gray-700">
+                        Peso (LB)*
+                      </label>
+                      <input
+                        type="number"
+                        name="namePackage"
+                        placeholder="Ej: 10 LB"
+                        required
+                        className="w-full bg-gray-200 text-black border border-gray-200 rounded py-3 px-4 mb-3"
+                      />
+                    </div>
+                    <div className="col-span-6 sm:col-span-3 lg:col-span-2">
+                      <label
+                        htmlFor="postal_code"
+                        className="block text-sm font-medium ">
+                        Cantidad (Unidad) *{" "}
+                      </label>
+                      <input
+                        type="number"
+                        name="namePackage"
+                        placeholder="Ej: 10"
+                        required
+                        className="w-full bg-gray-200 text-black border border-gray-200 rounded py-3 px-4 mb-3"
+                      />
+                    </div>
+                    <div className="col-span-6">
+                      <label
+                        htmlFor="street_address"
+                        className="block text-sm font-medium ">
+                        Ubicación del paquete{" "}
+                      </label>
+                    </div>
+                    <div className="col-span-6 sm:col-span-6 lg:col-span-2">
+                      <label
+                        htmlFor="city"
+                        className="block text-sm font-medium text-gray-700">
+                        Provincia*
+                      </label>
+                      <input
+                        type="text"
+                        name="namePackage"
+                        placeholder="Ej: Mesa Gaming"
+                        required
+                        className="w-full bg-gray-200 text-black border border-gray-200 rounded py-3 px-4 mb-3"
+                      />
+                    </div>
+                    <div className="col-span-6 sm:col-span-3 lg:col-span-2">
+                      <label
+                        htmlFor="state"
+                        className="block text-sm font-medium text-gray-700">
+                        Municipio*
+                      </label>
+                      <input
+                        type="number"
+                        name="namePackage"
+                        placeholder="Ej: 10 LB"
+                        required
+                        className="w-full bg-gray-200 text-black border border-gray-200 rounded py-3 px-4 mb-3"
+                      />
+                    </div>
+                    <div className="col-span-6 sm:col-span-3 lg:col-span-2">
+                      <label
+                        htmlFor="postal_code"
+                        className="block text-sm font-medium text-gray-700">
+                        Numero casa*{" "}
+                      </label>
+                      <input
+                        type="number"
+                        name="namePackage"
+                        placeholder="Ej: 10"
+                        required
+                        className="w-full bg-gray-200 text-black border border-gray-200 rounded py-3 px-4 mb-3"
+                      />
+                    </div>
+                    <div className="col-span-6">
+                      <label
+                        htmlFor="street_address"
+                        className="block text-sm font-medium ">
+                        Información del usuario final{" "}
+                      </label>
+                    </div>
+                    <div className="col-span-6 sm:col-span-6 lg:col-span-2">
+                      <label
+                        htmlFor="city"
+                        className="block text-sm font-medium text-gray-700">
+                        Nombre*
+                      </label>
+                      <input
+                        type="text"
+                        name="namePackage"
+                        placeholder="Ej: Mesa Gaming"
+                        required
+                        className="w-full bg-gray-200 text-black border border-gray-200 rounded py-3 px-4 mb-3"
+                      />
+                    </div>
+                    <div className="col-span-6 sm:col-span-3 lg:col-span-2">
+                      <label
+                        htmlFor="state"
+                        className="block text-sm font-medium text-gray-700">
+                        Apellido*
+                      </label>
+                      <input
+                        type="number"
+                        name="namePackage"
+                        placeholder="Ej: 10 LB"
+                        required
+                        className="w-full bg-gray-200 text-black border border-gray-200 rounded py-3 px-4 mb-3"
+                      />
+                    </div>
+                    <div className="col-span-6 sm:col-span-3 lg:col-span-1">
+                      <label
+                        htmlFor="postal_code"
+                        className="block text-sm font-medium text-gray-700">
+                        Sexo*{" "}
+                      </label>
+                      <select
+                        type="number"
+                        name="namePackage"
+                        placeholder="Ej: 10"
+                        required
+                        className="w-full bg-gray-200 text-black border border-gray-200 rounded py-3 px-4 mb-3"
+                      >
+                        <option>Masculino</option>
+                        <option>Femenino</option>
+                      </select>
+                    </div>
+                    <div className="col-span-6 sm:col-span-3 lg:col-span-1">
+                      <label
+                        htmlFor="postal_code"
+                        className="block text-sm font-medium text-gray-700">
+                        Celular*{" "}
+                      </label>
+                      <input
+                        type="number"
+                        name="namePackage"
+                        placeholder="Ej: 10"
+                        required
+                        className="w-full bg-gray-200 text-black border border-gray-200 rounded py-3 px-4 mb-3"
+                      />
+                    </div>
+                    <div className="col-span-6">
+                      <label
+                        htmlFor="street_address"
+                        className="block text-sm font-medium ">
+                        Ubicación de destino{" "}
+                      </label>
+                    </div>
+                    <div className="col-span-6 sm:col-span-6 lg:col-span-2">
+                      <label
+                        htmlFor="city"
+                        className="block text-sm font-medium text-gray-700">
+                        Provincia*
+                      </label>
+                      <input
+                        type="text"
+                        name="namePackage"
+                        placeholder="Ej: Mesa Gaming"
+                        required
+                        className="w-full bg-gray-200 text-black border border-gray-200 rounded py-3 px-4 mb-3"
+                      />
+                    </div>
+                    <div className="col-span-6 sm:col-span-3 lg:col-span-2">
+                      <label
+                        htmlFor="state"
+                        className="block text-sm font-medium text-gray-700">
+                        Municipio*
+                      </label>
+                      <input
+                        type="number"
+                        name="namePackage"
+                        placeholder="Ej: 10 LB"
+                        required
+                        className="w-full bg-gray-200 text-black border border-gray-200 rounded py-3 px-4 mb-3"
+                      />
+                    </div>
+                    <div className="col-span-6 sm:col-span-3 lg:col-span-2">
+                      <label
+                        htmlFor="postal_code"
+                        className="block text-sm font-medium text-gray-700">
+                        Numero casa*{" "}
+                      </label>
+                      <input
+                        type="number"
+                        name="namePackage"
+                        placeholder="Ej: 10"
+                        required
+                        className="w-full bg-gray-200 text-black border border-gray-200 rounded py-3 px-4 mb-3"
+                      />
+                    </div>
+                  </div>
+                </div>
+                <div className="px-4 py-3 bg-gray-50 text-right sm:px-6">
+                  <button
+                    type="submit"
+                    className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                    Guardar
+                  </button>
+                </div>
               </div>
-
-              <button
-                type="submit"
-                className="text-white mt-3 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-              >
-                Agregar paquete
-              </button>
-            </div>
-          )}
-        </form>
-        {viewForm && <UserFinalForms />}
+            </form>
+          </div>
+        </div>
       </div>
-    </>
+    </div>
   );
-}
+};
 
 export default PackageForms;
