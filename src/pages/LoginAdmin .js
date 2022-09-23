df3e33fdc42cdbceeed633730b7e4e811f788e69
Edward Brito Diaz <37183622+Edwardb11@users.noreply.github.com>
2022-09-23 15:41:54 -0400

    fix

--- src/pages/LoginAdmin .js
+++ src/pages/LoginAdmin .js
@@ -2,11 +2,7 @@ import React from "react";
 import Login from "../components/Login";
 
 const LoginAdmin = () => {
-  return (
-    <>
-      <Login admin={true} />
-    </>
-  );
+  return <Login admin={true} />;
 };
 
 export default LoginAdmin;
