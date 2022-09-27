6cebdbaca0988fa396aea0ea3e7682a1f66c0882
Edward Brito Diaz <37183622+Edwardb11@users.noreply.github.com>
2022-09-27 11:26:32 -0400

    update: about

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,10 +1,11 @@
 import React from "react";
 import Footer from "../components/Footer";
 import NavBar from "../components/NavBar";
-import fondo from "../img/fondo.jpeg";
-const About = () => {
+import fondo from "../img/fondo.webp";
+
+const Abouts = () => {
   return (
-    <>
+    <div>
       <NavBar />
       <link
         rel="stylesheet"
@@ -12,23 +13,36 @@ const About = () => {
       />
       <link
         rel="stylesheet"
-        href="https://use.fontawesome.com/releases/v5.0.8/css/solid.css"
+        href="https://demos.creative-tim.com/notus-js/assets/vendor/@fortawesome/fontawesome-free/css/all.min.css"
       />
-      <div className="mt-20">
-        <section className="relative  h-500-px  ">
+      <section className="relative  bg-blueGray-50">
+        <div className="relative pt-16 pb-32 flex content-center items-center justify-center min-h-screen-75">
           <div
             className="absolute top-0 w-full h-full bg-center bg-cover"
-            style={{ backgroundImage: `url(${fondo})` }}
-          >
+            style={{ backgroundImage: `url(${fondo})` }}>
             <span
               id="blackOverlay"
-              className="w-full h-full absolute opacity-50 bg-black"
+              className="w-full h-full absolute opacity-75 bg-black"
             />
           </div>
+          <div className="container relative mx-auto">
+            <div className="items-center flex flex-wrap">
+              <div className="w-full lg:w-6/12 px-4 ml-auto mr-auto text-center">
+                <div className="pr-12">
+                  <h1 className="text-white font-semibold text-5xl">
+                    Sistema de paquetería
+                  </h1>
+                  <p className="mt-4 text-lg text-blueGray-200">
+                    Somos una empresa de envios de paquetes comprometida con sus
+                    clientes para darle el mejor servicio.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
           <div
-            className="top-auto bottom-0 left-0 right-0 w-full absolute  pointer-events-none overflow-hidden h-70-px"
-            style={{ transform: "translateZ(0px)" }}
-          >
+            className="top-auto bottom-0 left-0 right-0 w-full absolute pointer-events-none overflow-hidden h-70-px"
+            style={{ transform: "translateZ(0px)" }}>
             <svg
               className="absolute bottom-0 overflow-hidden"
               xmlns="http://www.w3.org/2000/svg"
@@ -36,59 +50,72 @@ const About = () => {
               version="1.1"
               viewBox="0 0 2560 100"
               x={0}
-              y={0}
-            >
+              y={0}>
               <polygon
                 className="text-blueGray-200 fill-current"
                 points="2560 0 2560 100 0 100"
               />
             </svg>
           </div>
-        </section>
-        <section className="relative py-16 bg-blueGray-200">
-          <div className="container mx-auto px-4 ">
-            <div className="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-xl rounded-lg -mt-64">
-              <div className="px-6">
-                <div className="text-center mt-12">
-                  <h3 className="text-4xl font-semibold leading-normal text-blueGray-700 mb-2">
-                    Sistema de paquetería
-                  </h3>
-                  <div className="text-sm leading-normal mt-0 mb-2 text-blueGray-400 font-bold uppercase">
-                    <i className="fas fa-map-marker-alt mr-2 text-lg text-blueGray-400" />
-                    Republica Dominicana
-                  </div>
-                  <div className="mb-2 text-blueGray-600 mt-10">
-                    <i className="fa-solid fa-phone mr-2 text-lg text-blueGray-400" />
-                    829-462-9992 - 849-856-4014
-                  </div>
-                  <div className="mb-2 text-blueGray-600">
-                    <i className="fas fa-message mr-2 text-lg text-blueGray-400" />
-                    Somos una empresa comprometida con sus clientes para darle
-                    el mejor servicio.
+        </div>
+        <section className="pb-10 bg-blueGray-200 -mt-24">
+          <div className="container mx-auto px-4">
+            <div className="flex flex-wrap">
+              <div className="lg:pt-12 pt-6 w-full md:w-4/12 px-4 text-center">
+                <div className="relative flex flex-col min-w-0 break-words bg-white w-full mb-8 shadow-lg rounded-lg">
+                  <div className="px-4 py-5 flex-auto">
+                    <div className="text-white p-3 text-center inline-flex items-center justify-center w-12 h-12 mb-5 shadow-lg rounded-full bg-red-400">
+                      <i className="fas fa-award" />
+                    </div>
+                    <h6 className="text-xl font-semibold">Misión</h6>
+                    <p className="mt-2 mb-4 text-blueGray-500">
+                      Que los clientes pueda obtener toda la información al
+                      instante relevante a su envio de su paquete ó recibo de
+                      paquete.
+                    </p>
                   </div>
                 </div>
-                <div className="mt-10 py-10 border-t border-blueGray-200 text-center">
-                  <div className="flex flex-wrap justify-center">
-                    <div className="w-full lg:w-9/12 px-4">
-                      <p className="mb-4 text-lg leading-relaxed text-blueGray-700">
-                        Ser la empresa líder en el ramo de servicios de
-                        paquetería, reconocida en el país como confiable por sus
-                        mejores prácticas, por sus procesos de excelencia, por
-                        sus niveles de competitividad y por la calidad de sus
-                        productos y servicios, en un mundo que de forma
-                        constante se comunica de manera diferente.
-                      </p>
+              </div>
+              <div className="w-full md:w-4/12 px-4 text-center">
+                <div className="relative flex flex-col min-w-0 break-words bg-white w-full mb-8 shadow-lg rounded-lg">
+                  <div className="px-4 py-5 flex-auto">
+                    <div className="text-white p-3 text-center inline-flex items-center justify-center w-12 h-12 mb-5 shadow-lg rounded-full bg-lightBlue-400">
+                      <i className="fas fa-retweet" />
                     </div>
+                    <h6 className="text-xl font-semibold">Empresa</h6>
+
+                    <p className="mt-2 mb-7 text-blueGray-500">
+                      Sistema de paquetería
+                      <br />
+                      <span>La Vega, Republica Dominicana</span>
+                      <br />
+                      <span>Tel: 829-462-9992 - 849-856-4014</span>
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <div className="pt-6 w-full md:w-4/12 px-4 text-center">
+                <div className="relative flex flex-col min-w-0 break-words bg-white w-full mb-8 shadow-lg rounded-lg">
+                  <div className="px-4 py-5 flex-auto">
+                    <div className="text-white p-3 text-center inline-flex items-center justify-center w-12 h-12 mb-5 shadow-lg rounded-full bg-emerald-400">
+                      <i className="fas fa-fingerprint" />
+                    </div>
+                    <h6 className="text-xl font-semibold">Visión</h6>
+                    <p className="mt-2 mb-4 text-blueGray-500">
+                      Ser la empresa líder en la rama de servicios de envio de
+                      paquetes, reconocida en el país como confiable por sus
+                      mejores prácticas y cuidado del mismo.
+                    </p>
                   </div>
                 </div>
               </div>
             </div>
           </div>
         </section>
-      </div>
+      </section>
       <Footer />
-    </>
+    </div>
   );
 };
 
-export default About;
+export default Abouts;
