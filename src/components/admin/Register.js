50fa2ecdb3790bc191c1015c1784c1ab69d67804
Edward Brito Diaz <37183622+Edwardb11@users.noreply.github.com>
2022-09-28 12:42:21 -0400

    fix: data in input

--- src/components/admin/Register.js
+++ src/components/admin/Register.js
@@ -6,154 +6,155 @@ const Register = () => {
       <div className="mt-10 sm:mt-0">
         <div className="md:grid md:grid-cols-1 md:gap-6">
           <div className="mt-5 md:mt-0 md:col-span-2">
-          <form action="#" method="POST">
-            <div className="shadow overflow-hidden sm:rounded-md">
-              <div className="px-4 py-5 bg-white sm:p-6">
-                <div className="grid grid-cols-6 gap-6">
-                <div className="col-span-6 sm:col-span-6 lg:col-span-2">
-                    <label
-                      htmlFor="amount"
-                      className="block text-sm font-medium text-gray-700">
-                      Nombre*
-                    </label>
-                    <input
-                      id="amount"
-                      type="number"
-                      name="amount"
-                      required
-                      className="w-full bg-gray-100 text-black border border-gray-200 rounded py-3 px-4 mb-3"
-                    />
-                  </div>
-                  <div className="col-span-6 sm:col-span-6 lg:col-span-2">
-                    <label
-                      htmlFor="amount"
-                      className="block text-sm font-medium text-gray-700">
-                      Apellido*
-                    </label>
-                    <input
-                      id="amount"
-                      type="number"
-                      name="amount"
-                      required
-                      className="w-full bg-gray-100 text-black border border-gray-200 rounded py-3 px-4 mb-3"
-                    />
-                  </div>
-                  <div className="col-span-6 sm:col-span-6 lg:col-span-2">
-                    <label
-                      htmlFor="street_address"
-                      className="block text-sm font-medium text-gray-700">
-                      Celular
-                    </label>
-                    <input
-                      id="amount"
-                      type="number"
-                      name="amount"
-                      required
-                      className="w-full bg-gray-100 text-black border border-gray-200 rounded py-3 px-4 mb-3"
-                    />
-                  </div>
-                  <div className="col-span-6 sm:col-span-6 lg:col-span-2">
-                    <label
-                      htmlFor="city"
-                      className="block text-sm font-medium text-gray-700">
-                      Sexo
-                    </label>
-                    <input
-                      id="amount"
-                      type="number"
-                      name="amount"
-                      required
-                      className="w-full bg-gray-100 text-black border border-gray-200 rounded py-3 px-4 mb-3"
-                    />
-                  </div>
-                  <div className="col-span-6 sm:col-span-3 lg:col-span-2">
-                    <label
-                      htmlFor="state"
-                      className="block text-sm font-medium text-gray-700">
-                      Niveles de estudio
-                    </label>
-                    <input
-                      id="amount"
-                      type="number"
-                      name="amount"
-                      required
-                      className="w-full bg-gray-100 text-black border border-gray-200 rounded py-3 px-4 mb-3"
-                    />
-                  </div>
-                  <div className="col-span-6 sm:col-span-3 lg:col-span-2">
-                    <label
-                      htmlFor="postal_code"
-                      className="block text-sm font-medium text-gray-700">
-                      Fecha de nacimiento
-                    </label>
-                    <input
-                      id="amount"
-                      type="number"
-                      name="amount"
-                      required
-                      className="w-full bg-gray-100 text-black border border-gray-200 rounded py-3 px-4 mb-3"
-                    />
-                  </div>
+            <form action="#" method="POST">
+              <div className="shadow overflow-hidden sm:rounded-md">
+                <div className="px-4 py-5 bg-white sm:p-6">
+                  <div className="grid grid-cols-6 gap-6">
+                    <div className="col-span-6 sm:col-span-6 lg:col-span-2">
+                      <label
+                        htmlFor="name"
+                        className="block text-sm font-medium text-gray-700">
+                        Nombre*
+                      </label>
+                      <input
+                        id="name"
+                        type="text"
+                        name="name"
+                        required
+                        className="w-full bg-gray-100 text-black border border-gray-200 rounded py-3 px-4 mb-3"
+                      />
+                    </div>
+                    <div className="col-span-6 sm:col-span-6 lg:col-span-2">
+                      <label
+                        htmlFor="lastName"
+                        className="block text-sm font-medium text-gray-700">
+                        Apellido*
+                      </label>
+                      <input
+                        id="lastName"
+                        type="text"
+                        name="lastName"
+                        required
+                        className="w-full bg-gray-100 text-black border border-gray-200 rounded py-3 px-4 mb-3"
+                      />
+                    </div>
+                    <div className="col-span-6 sm:col-span-6 lg:col-span-2">
+                      <label
+                        htmlFor="phone"
+                        className="block text-sm font-medium text-gray-700">
+                        Celular
+                      </label>
+                      <input
+                        id="phone"
+                        type="text"
+                        name="phone"
+                        required
+                        className="w-full bg-gray-100 text-black border border-gray-200 rounded py-3 px-4 mb-3"
+                      />
+                    </div>
+                    <div className="col-span-6 sm:col-span-6 lg:col-span-2">
+                      <label
+                        htmlFor="sex"
+                        className="block text-sm font-medium text-gray-700">
+                        Sexo
+                      </label>
+                      <select
+                        name="sex"
+                        className="w-full bg-gray-100 text-black border border-gray-200 rounded py-3 px-4 mb-3"
+                        required>
+                        <option value="m">Masculino</option>
+                        <option value="f">Femenino</option>
+                      </select>
+                    </div>
+                    <div className="col-span-6 sm:col-span-3 lg:col-span-2">
+                      <label
+                        htmlFor="studyLevels"
+                        className="block text-sm font-medium text-gray-700">
+                        Niveles de estudio
+                      </label>
+                      <select
+                        name="studyLevels"
+                        className="w-full bg-gray-100 text-black border border-gray-200 rounded py-3 px-4 mb-3"
+                        required>
+                        <option value="m">Primario</option>
+                        <option value="f">Secundarios</option>
+                        <option value="f">Universitarios</option>
+                        <option value="f">Tecnicos</option>
+                      </select>
+                    </div>
+                    <div className="col-span-6 sm:col-span-3 lg:col-span-2">
+                      <label
+                        htmlFor="birthDate"
+                        className="block text-sm font-medium text-gray-700">
+                        Fecha de nacimiento
+                      </label>
+                      <input
+                        id="birthDate"
+                        type="date"
+                        name="birthDate"
+                        required
+                        className="w-full bg-gray-100 text-black border border-gray-200 rounded py-3 px-4 mb-3"
+                      />
+                    </div>
 
-                  <div className="col-span-6 sm:col-span-3">
-                    <label
-                      htmlFor="street_address"
-                      className="block text-sm font-medium text-gray-700">
-                      Correo Electrónico
-                    </label>
-                    <input
-                      id="amount"
-                      type="number"
-                      name="amount"
-                      required
-                      className="w-full bg-gray-100 text-black border border-gray-200 rounded py-3 px-4 mb-3"
-                    />
-                  </div>
-                  <div className="col-span-6 sm:col-span-3">
-                    <label
-                      htmlFor="street_address"
-                      className="block text-sm font-medium text-gray-700">
-                      Contraseña
-                    </label>
-                    <input
-                      id="amount"
-                      type="number"
-                      name="amount"
-                      required
-                      className="w-full bg-gray-100 text-black border border-gray-200 rounded py-3 px-4 mb-3"
-                    />
-                  </div>
+                    <div className="col-span-6 sm:col-span-3">
+                      <label
+                        htmlFor="email"
+                        className="block text-sm font-medium text-gray-700">
+                        Correo Electrónico
+                      </label>
+                      <input
+                        id="email"
+                        type="email"
+                        name="email"
+                        required
+                        className="w-full bg-gray-100 text-black border border-gray-200 rounded py-3 px-4 mb-3"
+                      />
+                    </div>
+                    <div className="col-span-6 sm:col-span-3">
+                      <label
+                        htmlFor="password"
+                        className="block text-sm font-medium text-gray-700">
+                        Contraseña
+                      </label>
+                      <input
+                        id="password"
+                        type="password"
+                        name="password"
+                        required
+                        className="w-full bg-gray-100 text-black border border-gray-200 rounded py-3 px-4 mb-3"
+                      />
+                    </div>
 
-                  <div className="col-span-6 sm:col-span-3">
-                    <label
-                      htmlFor="paymentMethod"
-                      className="block text-sm font-medium text-gray-700">
-                      Rol*
-                    </label>
-                    <select
-                      id="paymentMethod"
-                      name="paymentMethod"
-                      required
-                      className="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
-                      <option value="1" defaultChecked>
-                        Adm
-                      </option>
-                    </select>
+                    <div className="col-span-6 sm:col-span-3">
+                      <label
+                        htmlFor="rol"
+                        className="block text-sm font-medium text-gray-700">
+                        Rol*
+                      </label>
+                      <select
+                        name="rol"
+                        className="w-full bg-gray-100 text-black border border-gray-200 rounded py-3 px-4 mb-3"
+                        required>
+                        <option value="3">Trabajador</option>
+                        <option value="2">Repartidor</option>
+                        <option value="1">Admin</option>
+                      </select>
+                    </div>
                   </div>
                 </div>
+                <div className="px-4 py-3 bg-gray-50 text-right sm:px-6">
+                  <button
+                    type="submit"
+                    className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                    Guardar
+                  </button>
+                </div>
               </div>
-              <div className="px-4 py-3 bg-gray-50 text-right sm:px-6">
-                <button
-                  type="submit"
-                  className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                  Save
-                </button>
-              </div>
-            </div>
-          </form>
+            </form>
+          </div>
+        </div>
       </div>
-    </div>
-    </div>
     </div>
   );
 };
