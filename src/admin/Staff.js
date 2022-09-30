a629501342eb421c8d89fad646d933bb524e6d2f
Edward Brito Diaz <37183622+Edwardb11@users.noreply.github.com>
2022-09-30 14:33:14 -0400

    fix

--- src/admin/Staff.js
+++ src/admin/Staff.js
@@ -7,7 +7,7 @@ const Staff = () => {
   return (
     <>
       <NavBar />
-      <div className="py-10">
+      <div className="py-10 mt-10">
         <div className="flex flex-col px-8 mx-auto space-y-12 max-w-7xl xl:px-12">
           <div className="relative">
             <h2 className="w-full text-3xl font-bold text-center sm:text-4xl md:text-5xl">
