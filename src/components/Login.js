4d8dfcd0530f8dac9f18d578fba9ad421ba6268f
Edward Brito Diaz <37183622+Edwardb11@users.noreply.github.com>
2022-09-30 13:42:26 -0400

    fix

--- src/components/Login.js
+++ src/components/Login.js
@@ -70,9 +70,7 @@ const Login = ({ admin }) => {
                       xmlns="http://www.w3.org/2000/svg"
                       className="w-6 h-6"
                       fill="none"
-                      viewBox="0 0 24 24"
-                      stroke="currentColor"
-                      stroke-width="2">
+                      viewBox="0 0 24 24">
                       <path d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
                     </svg>
 
@@ -91,9 +89,7 @@ const Login = ({ admin }) => {
                       xmlns="http://www.w3.org/2000/svg"
                       className="w-6 h-6"
                       fill="none"
-                      viewBox="0 0 24 24"
-                      stroke="currentColor"
-                      stroke-width="2">
+                      viewBox="0 0 24 24">
                       <path d="M21 13.255A23.931 23.931 0 0112 15c-3.183 0-6.22-.62-9-1.745M16 6V4a2 2 0 00-2-2h-4a2 2 0 00-2 2v2m4 6h.01M5 20h14a2 2 0 002-2V8a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
                     </svg>
                     <span className="mx-2">Admin</span>
