6899ae56f97f243fd9ea21412c7205a0fb16ef9b
Edward Brito Diaz <37183622+Edwardb11@users.noreply.github.com>
2022-10-07 15:30:02 -0400

    feat: layout data in table

--- src/components/admin/TablePackage.js
+++ src/components/admin/TablePackage.js
@@ -46,6 +46,7 @@ const TablePackage = () => {
   const result = filters.filter((item, index) => {
     return filters.indexOf(item) === index;
   });
+
   return (
     <div className="w-full xl:w-8/12 mb-12 xl:mb-0 px-4 mx-auto mt-24">
       <div className="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded ">
@@ -90,11 +91,14 @@ const TablePackage = () => {
                 <th className="px-6 bg-blueGray-50 text-blueGray-500 align-middle border border-solid border-blueGray-100 py-3 text-md   border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
                   Peso
                 </th>
-                <th className="px-6 bg-blueGray-50 text-blueGray-500 align-middle border border-solid border-blueGray-100 py-3 text-md  border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
-                  Destino
-                </th>
                 <th className="px-6 bg-blueGray-50 text-blueGray-500 align-middle border border-solid border-blueGray-100 py-3 text-md   border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
                   Estado
+                </th>
+                <th className="px-6 bg-blueGray-50 text-blueGray-500 align-middle border border-solid border-blueGray-100 py-3 text-md   border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
+                  Buscar
+                </th>
+                <th className="px-6 bg-blueGray-50 text-blueGray-500 align-middle border border-solid border-blueGray-100 py-3 text-md   border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
+                  Destino
                 </th>
                 <th className="px-6 bg-blueGray-50 text-blueGray-500 align-middle border border-solid border-blueGray-100 py-3 text-md   border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
                   Actualizar estado
@@ -107,17 +111,22 @@ const TablePackage = () => {
                 return (
                   <tr key={key}>
                     <td className="border-t-0 px-6  border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-                      {item?.id_estado}
-                    </td>
-                    <td className="border-t-0 px-6  border-l-0 border-r-0 text-xs whitespace-nowrap p-4"></td>
-                    <td className="border-t-0 px-6  border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-                      CAMBIAR{" "}
+                      {item?.paquete.id_paquete}
                     </td>
                     <td className="border-t-0 px-6  border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-                      CAMBIAR{" "}
+                      {item?.paquete.nombre}
                     </td>
                     <td className="border-t-0 px-6  border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-                      CAMBIAR{" "}
+                      {item?.paquete.peso}
+                    </td>
+                    <td className="border-t-0 px-6 font-semibold bg-gray-100  border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                      {item?.estado?.nombre || "PENDIENTE"}
+                    </td>
+                    <td className="border-t-0  px-6  border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                      {item?.paquete?.ubicacion}
+                    </td>
+                    <td className="border-t-0 px-6  border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                      {item?.paquete?.usuario_finale?.ubicacion}
                     </td>
                     <td className="border-t-0 px-6  border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
                       <button className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
