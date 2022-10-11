42ff192cb81c38202abf9c5995aecad6bacd56d4
Edward Brito Diaz <37183622+Edwardb11@users.noreply.github.com>
2022-10-11 14:26:45 -0400

    feat: package ready table

--- src/components/admin/PackageReady.js
+++ src/components/admin/PackageReady.js
@@ -1,6 +1,10 @@
-import React from 'react'
+import React from "react";
+import usePackageReady from "../../hooks/usePackageReady";
+import convertDate from "../../utils/convertDate";
 
 const PackageReady = () => {
+  const data = usePackageReady();
+  const state = data.data;
   return (
     <div className="w-full xl:w-10/12 mb-12 xl:mb-0 px-4 mx-auto mt-24">
       <div className="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded ">
@@ -32,25 +36,24 @@ const PackageReady = () => {
               </tr>
             </thead>
 
-            {/* <tbody>
-              {result.map((item, key) => {
+            <tbody>
+              {state.map((item, key) => {
+                console.log(item);
                 return (
                   <tr key={key}>
-                    <td className="border-t-0 px-6 text-md text-blue-800 font-semibold  border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                    <td className="border-t-0 px-10  text-md text-blue-800 font-semibold  border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
                       {item?.paquete.id_paquete}
                     </td>
                     <td className="border-t-0 px-6  border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
                       {item?.paquete.nombre}
                     </td>
                     <td className="border-t-0 px-6  border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-                      {item?.paquete.peso } LB
+                      {item?.paquete.peso} LB
                     </td>
                     <td className="border-t-0 px-6 font-semibold bg-gray-100  border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-                      {item?.estado?.nombre || "PENDIENTE"}
-                    </td>
-                    <td className="border-t-0 px-6   border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
                       {convertDate(item?.creado)}
                     </td>
+
                     <td className="border-t-0  px-6  border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
                       {item?.paquete?.ubicacion}
                     </td>
@@ -58,21 +61,17 @@ const PackageReady = () => {
                       {item?.paquete?.usuario_finale?.ubicacion}
                     </td>
                     <td className="border-t-0 px-6  border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-                      <Link
-                        to={`/admin/statePackage/${item?.paquete.id_paquete}`}
-                        className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                        Actualizar
-                      </Link>
+                      {item?.paquete?.facturas[0]?.cantidad_a_pagar} RD$
                     </td>
                   </tr>
                 );
               })}
-            </tbody> */}
+            </tbody>
           </table>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default PackageReady
+export default PackageReady;
