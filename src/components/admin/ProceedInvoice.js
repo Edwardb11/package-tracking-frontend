4e623d3659857a98f72ef3a01979470298a14ab6
Edward Brito Diaz <37183622+Edwardb11@users.noreply.github.com>
2022-10-13 11:50:17 -0400

    feat: modal proced add invoice

--- src/components/admin/ProceedInvoice.js
+++ src/components/admin/ProceedInvoice.js
@@ -1,36 +1,76 @@
-import React from "react";
+import React, { useState } from "react";
 
-const ProceedInvoice = ({ tracking, setData }) => {
+const ProceedInvoice = ({ tracking, setData, modal }) => {
+  const [showModal, setShowModal] = useState(modal);
+
   return (
-    <form className="shadow-lg ">
-      <div className="shadow overflow-hidden sm:rounded-md ">
-        <div className="px-4 py-5 bg-white sm:p-6 ">
-          <div className="grid grid-cols-6 gap-6">
-            <div className="col-span-6 sm:col-span-3">
-              <label
-                htmlFor="amount"
-                className="block text-sm font-medium text-gray-700">
-                Monto*
-              </label>
-              <input
-                id="amount"
-                type="number"
-                name="amount"
-                required
-                className="w-full bg-gray-50 text-black border border-gray-200 rounded py-3 px-4 mb-3"
-              />
+    <>
+      {showModal ? (
+        <>
+          <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
+            <div className="relative w-auto my-6 mx-auto max-w-3xl">
+              {/*content*/}
+              <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+                {/*header*/}
+                <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
+                  <h3 className="text-3xl font-semibold">
+                    Facturar: {tracking}
+                  </h3>
+                  <button
+                    className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
+                    onClick={() => setShowModal(false)}>
+                    <span className="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
+                      ×
+                    </span>
+                  </button>
+                </div>
+                {/*body*/}
+                <div className="relative p-6 flex-auto">
+                  <form className="shadow-lg ">
+                    <div className="shadow overflow-hidden sm:rounded-md ">
+                      <div className="px-4 py-5 bg-white sm:p-6 ">
+                        <div className="grid grid-cols-6 gap-6">
+                          <div className="col-span-6 sm:col-span-3">
+                            <label
+                              htmlFor="amount"
+                              className="block text-sm font-medium text-gray-700">
+                              Monto*
+                            </label>
+                            <input
+                              id="amount"
+                              type="number"
+                              name="amount"
+                              required
+                              className="w-full bg-gray-50 text-black border border-gray-200 rounded py-3 px-4 mb-3"
+                            />
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </form>
+                </div>
+                {/*footer*/}
+                <div className="flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b">
+                  <button
+                    className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                    type="button"
+                    onClick={() => setShowModal(false)}>
+                    Cerrar
+                  </button>
+                  <button
+                    className="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                    type="button"
+                    onClick={() => setShowModal(false)}>
+                    Guardar
+                  </button>
+                </div>
+              </div>
             </div>
           </div>
-        </div>
-        <div className="px-4 py-3 bg-gray-50 text-right sm:px-6">
-          <button
-            type="submit"
-            className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-            Guardar
-          </button>
-        </div>
-      </div>
-    </form>
+          <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+        </>
+      ) : null}
+    </>
   );
 };
 
