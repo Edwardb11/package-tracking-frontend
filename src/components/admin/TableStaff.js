7bc6f3af423422f76e323e0a1da936792b14eeef
Edward Brito Diaz <37183622+Edwardb11@users.noreply.github.com>
2022-10-14 15:04:26 -0400

    update: design table list staff

--- src/components/admin/TableStaff.js
+++ src/components/admin/TableStaff.js
@@ -8,71 +8,78 @@ const TableStaff = () => {
 
   const { user } = useContext(User);
   return (
-    <div className="overflow-x-auto relative lg:mx-20 xl:mx-10">
-      <table className="w-full border">
-        <thead>
-          <tr className="bg-gray-200 border-r">
-            <th className="p-2 border-r cursor-pointer text-base  text-gray-500">
-              <div className="flex items-center justify-center">
-                Nombre completo
-              </div>
-            </th>
-            <th className="p-2 border-r cursor-pointer text-base text-gray-500">
-              <div className="flex items-center justify-center">E-mail</div>
-            </th>
-            <th className="p-2 border-r cursor-pointer text-base  text-gray-500">
-              <div className="flex items-center justify-center">Celular</div>
-            </th>
-            <th className="p-2 border-r cursor-pointer text-base text-gray-500">
-              <div className="flex items-center justify-center">Rol</div>
-            </th>
-            <th className="p-2 border-r cursor-pointer text-base text-gray-500">
-              <div className="flex items-center justify-center">Accion</div>
-            </th>
-          </tr>
-        </thead>
-        <tbody>
-          {data.data.map((items) => {
-            return (
-              <>
-                <tr
-                  className=" text-center border-b text-sm text-gray-600"
-                  key={items.personal.id_personal}>
-                  <td className="p-2 border-r">
-                    {items.personal.nombres} {items.personal.apellidos}
-                  </td>
-                  <td className="p-2 border-r">
-                    {items.personal.correo_electronico}
-                  </td>
-                  <td className="p-2 border-r">
-                    {items.personal.celular || "No definido"}
-                  </td>
-                  <td className="p-2 border-r">
-                    -- {items.personal.roles[0].nombre} -{" "}
-                    {items.personal?.roles[1]?.nombre} -{" "}
-                    {items.personal?.roles[2]?.nombre}
-                  </td>
-                  <td>
-                    <button
-                      disabled={items.personal?.id_personal === user.staffId}
-                      onClick={() => removeStaff(items.personal.id_personal,setData)}
-                      className={`inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md 
+    <div className="w-full xl:w-10/12 mb-12 xl:mb-0 px-4 mx-auto mt-24">
+      <div className="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded ">
+        <div className="block w-full overflow-x-auto">
+          <table className="items-center bg-transparent w-full border-collapse ">
+            <thead>
+              <tr className="bg-gray-200">
+                <th className="px-6 align-middle border border-solid py-3  text-md  border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
+                  Nombre completo
+                </th>
+
+                <th className="px-6 align-middle border border-solid  py-3  text-md  border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
+                  Correo Electronico
+                </th>
+                <th className="px-6 align-middle border border-solid  py-3  text-md  border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
+                  Celular
+                </th>
+                <th className="px-6 align-middle border border-solid  py-3  text-md  border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
+                  Rol
+                </th>
+                <th className="px-6 align-middle border border-solid border-blueGray-100 py-3  text-md  border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
+                  Acciones
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              {data.data.map((items) => {
+                return (
+                  <>
+                    <tr
+                      className=" border-b text-sm text-gray-600"
+                      key={items.personal.id_personal}>
+                      <td className=" px-6 text-md  p-2 border-r whitespace-nowrap p-4">
+                        {items.personal.nombres} {items.personal.apellidos}
+                      </td>
+                      <td className=" px-6 p-2 border-r text-md whitespace-nowrap p-4">
+                        {items.personal.correo_electronico}
+                      </td>
+                      <td className=" px-6  p-2 border-r text-md whitespace-nowrap p-4">
+                        {items.personal.celular || "No definido"}
+                      </td>
+                      <td className=" px-6 font-bold  p-2 border-r text-md whitespace-nowrap p-4">
+                        -- {items.personal.roles[0].nombre} -{" "}
+                        {items.personal?.roles[1]?.nombre} -{" "}
+                        {items.personal?.roles[2]?.nombre}
+                      </td>
+                      <td>
+                        <button
+                          disabled={
+                            items.personal?.id_personal === user.staffId
+                          }
+                          onClick={() =>
+                            removeStaff(items.personal.id_personal, setData)
+                          }
+                          className={`inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md 
                       ${
                         items.personal?.id_personal === user.staffId
                           ? " bg-gray-300  hover:bg-gra-100 cursor-not-allowed "
                           : " text-white bg-red-600 hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500"
                       }
                       `}>
-                      Eliminar
-                    </button>
-                  </td>
-                </tr>
-              </>
-            );
-          })}
-          <tr className="bg-gray-100 text-center border-b text-sm text-gray-600"></tr>
-        </tbody>
-      </table>
+                          Eliminar
+                        </button>
+                      </td>
+                    </tr>
+                  </>
+                );
+              })}
+              <tr className="bg-gray-100 text-center border-b text-sm text-gray-600"></tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
     </div>
   );
 };
