0be7b9184b9f59bbecc79aa2cf656a789b091a7a
Edward Brito Diaz <37183622+Edwardb11@users.noreply.github.com>
2022-10-18 13:23:57 -0400

    fix

--- src/components/admin/EditRol.js
+++ src/components/admin/EditRol.js
@@ -24,12 +24,15 @@ const EditRol = ({ id, setData, showModal, setShowModals }) => {
       {showModal ? (
         <>
           <div className="justify-center  items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
-            <div className=" my-6 mx-auto w-2/4  ">
+            <div className=" my-6 mx-auto 2xl:2/4 xl:w-2/4  lg:2/4 md:1/2 ">
               {/*content*/}
               <div className="border-0 rounded-lg w-full   shadow-lg relative flex flex-col  bg-white outline-none focus:outline-none">
                 {/*header*/}
                 <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
-                  <h3 className="text-3xl font-semibold">ID: {id}</h3>
+                  <h3 className="text-3xl font-semibold">
+                    ID: {id} - {state[0].personal?.nombres}{" "}
+                    {state[0].personal?.apellidos}
+                  </h3>
                 </div>
                 {/*body*/}
                 <div className="relative p-6 flex-auto">
