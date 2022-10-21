b891b2bcc24670908e8052a3cef4fe4f676226eb
Edward Brito Diaz <37183622+Edwardb11@users.noreply.github.com>
2022-10-21 13:38:52 -0400

    update design for add package

--- src/components/client/PackageForms.js
+++ src/components/client/PackageForms.js
@@ -53,17 +53,13 @@ const PackageForms = () => {
   };
 
   return (
-    <div className="bg-gray-100 mx-auto max-w-6xl bg-white py-20 px-12 lg:px-24 shadow-xl mb-24">
+    <div className=" mx-auto max-w-7xl bg-white py-20 px-12 lg:px-24 shadow-xl mb-24">
       <div className="mt-10 sm:mt-0">
         <div className="md:grid md:grid-cols-1 md:gap-6">
           <div className="mt-5 md:mt-0 md:col-span-2">
             <form onSubmit={Validate}>
-              <div className="shadow overflow-hidden sm:rounded-md">
-                <h1 className="text-center font-medium text-xl">
-                  Complete el siguiente formulario para agregar un paquete
-                </h1>
+              <div className=" overflow-hidden sm:rounded-md">
                 <div className="w-full h-0.5 bg-indigo-500" />
-
                 <div className="px-2 py-2 bg-white sm:p-6">
                   <div className="grid grid-cols-6 gap-6">
                     <div className="col-span-6">
@@ -86,7 +82,7 @@ const PackageForms = () => {
                         onChange={(e) => setNamePackage(e.target.value)}
                         placeholder="Ej: Mesa Gaming"
                         required
-                        className="w-full bg-gray-200 text-black border border-gray-200 rounded py-3 px-4 mb-3"
+                        className="w-full bg-gray-50 text-black border border-gray-200 rounded py-3 px-4 mb-3"
                       />
                     </div>
                     <div className="col-span-6 sm:col-span-3 lg:col-span-2">
@@ -103,7 +99,7 @@ const PackageForms = () => {
                         onChange={(e) => setWeightPackage(e.target.value)}
                         placeholder="Ej: 10 LB"
                         required
-                        className="w-full bg-gray-200 text-black border border-gray-200 rounded py-3 px-4 mb-3"
+                        className="w-full bg-gray-50 text-black border border-gray-200 rounded py-3 px-4 mb-3"
                       />
                     </div>
                     <div className="col-span-6 sm:col-span-3 lg:col-span-2">
@@ -119,7 +115,7 @@ const PackageForms = () => {
                         value={quantityPackage}
                         onChange={(e) => setQuantityPackage(e.target.value)}
                         required
-                        className="w-full bg-gray-200 text-black border border-gray-200 rounded py-3 px-4 mb-3">
+                        className="w-full bg-gray-50 text-black border border-gray-200 rounded py-3 px-4 mb-3">
                         {Quantitys.map((item) => {
                           return <option value={item}>{item}</option>;
                         })}
@@ -145,7 +141,7 @@ const PackageForms = () => {
                         onChange={(e) => setProvinceLocated(e.target.value)}
                         placeholder="Ej: La Vega"
                         required
-                        className="w-full bg-gray-200 text-black border border-gray-200 rounded py-3 px-4 mb-3"
+                        className="w-full bg-gray-50 text-black border border-gray-200 rounded py-3 px-4 mb-3"
                       />
                     </div>
                     <div className="col-span-6 sm:col-span-3 lg:col-span-2">
@@ -161,7 +157,7 @@ const PackageForms = () => {
                         onChange={(e) => setMunicipalityLocated(e.target.value)}
                         placeholder="Ej: Rio verde arriba"
                         required
-                        className="w-full bg-gray-200 text-black border border-gray-200 rounded py-3 px-4 mb-3"
+                        className="w-full bg-gray-50 text-black border border-gray-200 rounded py-3 px-4 mb-3"
                       />
                     </div>
                     <div className="col-span-6 sm:col-span-3 lg:col-span-2">
@@ -178,7 +174,7 @@ const PackageForms = () => {
                         onChange={(e) => setHouseNumberLocated(e.target.value)}
                         placeholder="Ej: 50"
                         required
-                        className="w-full bg-gray-200 text-black border border-gray-200 rounded py-3 px-4 mb-3"
+                        className="w-full bg-gray-50 text-black border border-gray-200 rounded py-3 px-4 mb-3"
                       />
                     </div>
                     <div className="col-span-6">
@@ -201,7 +197,7 @@ const PackageForms = () => {
                         onChange={(e) => setNameUser(e.target.value)}
                         placeholder="Ej: Juan"
                         required
-                        className="w-full bg-gray-200 text-black border border-gray-200 rounded py-3 px-4 mb-3"
+                        className="w-full bg-gray-50 text-black border border-gray-200 rounded py-3 px-4 mb-3"
                       />
                     </div>
                     <div className="col-span-6 sm:col-span-3 lg:col-span-2">
@@ -217,7 +213,7 @@ const PackageForms = () => {
                         onChange={(e) => setLastNameUser(e.target.value)}
                         placeholder="Ej: Perez Diaz"
                         required
-                        className="w-full bg-gray-200 text-black border border-gray-200 rounded py-3 px-4 mb-3"
+                        className="w-full bg-gray-50 text-black border border-gray-200 rounded py-3 px-4 mb-3"
                       />
                     </div>
                     <div className="col-span-6 sm:col-span-3 lg:col-span-1">
@@ -231,7 +227,7 @@ const PackageForms = () => {
                         value={sexUser}
                         onChange={(e) => setSexUser(e.target.value)}
                         required
-                        className="w-full bg-gray-200 text-black border border-gray-200 rounded py-3 px-4 mb-3">
+                        className="w-full bg-gray-50 text-black border border-gray-200 rounded py-3 px-4 mb-3">
                         <option value="m">Masculino</option>
                         <option value="f">Femenino</option>
                       </select>
@@ -249,7 +245,7 @@ const PackageForms = () => {
                         onChange={(e) => setPhoneUser(e.target.value)}
                         placeholder="Ej: 809-293-9028"
                         required
-                        className="w-full bg-gray-200 text-black border border-gray-200 rounded py-3 px-4 mb-3"
+                        className="w-full bg-gray-50 text-black border border-gray-200 rounded py-3 px-4 mb-3"
                       />
                     </div>
                     <div className="col-span-6">
@@ -272,7 +268,7 @@ const PackageForms = () => {
                         onChange={(e) => setDestinationProvince(e.target.value)}
                         placeholder="Ej: Azua"
                         required
-                        className="w-full bg-gray-200 text-black border border-gray-200 rounded py-3 px-4 mb-3"
+                        className="w-full bg-gray-50 text-black border border-gray-200 rounded py-3 px-4 mb-3"
                       />
                     </div>
                     <div className="col-span-6 sm:col-span-3 lg:col-span-2">
@@ -290,7 +286,7 @@ const PackageForms = () => {
                           setDestinationMunicipality(e.target.value)
                         }
                         required
-                        className="w-full bg-gray-200 text-black border border-gray-200 rounded py-3 px-4 mb-3"
+                        className="w-full bg-gray-50 text-black border border-gray-200 rounded py-3 px-4 mb-3"
                       />
                     </div>
                     <div className="col-span-6 sm:col-span-3 lg:col-span-2">
@@ -307,7 +303,7 @@ const PackageForms = () => {
                         onChange={(e) => setHouseDestination(e.target.value)}
                         placeholder="Ej: 10"
                         required
-                        className="w-full bg-gray-200 text-black border border-gray-200 rounded py-3 px-4 mb-3"
+                        className="w-full bg-gray-50 text-black border border-gray-200 rounded py-3 px-4 mb-3"
                       />
                     </div>
                   </div>
