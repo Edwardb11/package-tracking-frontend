c619fa9a260cbef758d7c14d12ac55a44f3e09ec
Edward Brito Diaz <37183622+Edwardb11@users.noreply.github.com>
2022-11-07 10:50:37 -0400

    feat: changes initial for dasboard

--- src/admin/Dashboard.js
+++ src/admin/Dashboard.js
@@ -2,11 +2,21 @@ import React from "react";
 import Footer from "../components/Footer";
 import NavBar from "../components/NavBar";
 
+import WelcomeBanner from "../dashboard/WelcomeBanner";
+
 const Dashboard = () => {
   return (
-    <div>
+    <div className="relative flex flex-col flex-1 overflow-y-auto overflow-x-hidden">
       <NavBar />
-      <div lassName="mt-20">Dashboard</div>
+      <main>
+        <div className="px-4 sm:px-6 lg:px-8 py-8 w-full max-w-9xl mx-auto">
+          {/* Welcome banner */}
+          <WelcomeBanner />
+          {/* Cards */}
+          <div className="grid grid-cols-12 gap-6"></div>
+        </div>
+      </main>
+
       <Footer />
     </div>
   );
