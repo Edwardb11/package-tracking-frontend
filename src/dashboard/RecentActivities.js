31987669d3906e4912eae2e9b3c7deb17eadf769
Edward Brito Diaz <37183622+Edwardb11@users.noreply.github.com>
2022-11-09 11:52:05 -0400

    update desing

--- src/dashboard/RecentActivities.js
+++ src/dashboard/RecentActivities.js
@@ -1,10 +1,11 @@
 import React from "react";
+import Card from "./Card";
 
 const RecentActivities = () => {
   return (
     <>
-      <section className="py-1 bg-blueGray-50">
-        <div className="w-full xl:w-8/12 mb-12 xl:mb-0 px-4 mx-auto mt-24">
+      <section className="flex flex-wrap py-1  mt-24">
+        <div className="w-8/12  xl:w-8/12 xl:mb-0 px-4 mx-auto ">
           <div className="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded ">
             <div className="rounded-t mb-0 px-4 py-3 border-0">
               <div className="flex flex-wrap items-center">
@@ -23,39 +24,48 @@ const RecentActivities = () => {
             <div className="block w-full overflow-x-auto">
               <table className="items-center bg-transparent w-full border-collapse ">
                 <thead>
-                  <tr>
-                    <th className="px-6 bg-blueGray-50 text-blueGray-500 align-middle border border-solid border-blueGray-100 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
+                       <tr className="bg-indigo-300 text-white">
+
+                       <th className="px-6 align-middle border border-solid py-3  text-md  border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
                       ID
                     </th>
-                    <th className="px-6 bg-blueGray-50 text-blueGray-500 align-middle border border-solid border-blueGray-100 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
+                    <th className="px-6 align-middle border border-solid py-3  text-md  border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
                       Personal
                     </th>
-                    <th className="px-6 bg-blueGray-50 text-blueGray-500 align-middle border border-solid border-blueGray-100 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
+                    <th className="px-6 align-middle border border-solid py-3  text-md  border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
                       Rol
                     </th>
-                    <th className="px-6 bg-blueGray-50 text-blueGray-500 align-middle border border-solid border-blueGray-100 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
+                    <th className="px-6 align-middle border border-solid py-3  text-md  border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
                       Actividad
                     </th>
-                    <th className="px-6 bg-blueGray-50 text-blueGray-500 align-middle border border-solid border-blueGray-100 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
+                    <th className="px-6 align-middle border border-solid py-3  text-md  border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
                       Paquete
                     </th>
-                    <th className="px-6 bg-blueGray-50 text-blueGray-500 align-middle border border-solid border-blueGray-100 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
+                    <th className="px-6 align-middle border border-solid py-3  text-md  border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
                       Fecha
                     </th>
                   </tr>
                 </thead>
                 <tbody>
                   <tr>
-                    <th className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-left text-blueGray-700 ">
+                    <th className=" px-6 p-2 border-r text-md whitespace-nowrap p-4">
                       /argon/
                     </th>
-                    <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 ">
+                    <td className=" px-6 p-2 border-r text-md whitespace-nowrap p-4">
                       4,569
                     </td>
-                    <td className="border-t-0 px-6 align-center border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                    <td className=" px-6 p-2 border-r text-md whitespace-nowrap p-4">
                       340
                     </td>
-                    <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                    <td className=" px-6 p-2 border-r text-md whitespace-nowrap p-4">
+                      <i className="fas fa-arrow-up text-emerald-500 mr-4" />
+                      46,53%
+                    </td>
+                    <td className=" px-6 p-2 border-r text-md whitespace-nowrap p-4">
+                      <i className="fas fa-arrow-up text-emerald-500 mr-4" />
+                      46,53%
+                    </td>
+                    <td className=" px-6 p-2 border-r text-md whitespace-nowrap p-4">
                       <i className="fas fa-arrow-up text-emerald-500 mr-4" />
                       46,53%
                     </td>
@@ -64,6 +74,10 @@ const RecentActivities = () => {
               </table>
             </div>
           </div>
+
+          </div>
+          <div className="flex w-1/5  ">
+            <Card title={` Ultimo paquete `} total="11" key={"1"}/>
         </div>
       </section>
     </>
