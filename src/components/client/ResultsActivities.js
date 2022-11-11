58261657b1fc590c8557a04b08e4d9d1e187e0cb
Edward Brito Diaz <37183622+Edwardb11@users.noreply.github.com>
2022-11-11 12:56:39 -0400

    fix

--- src/components/client/ResultsActivities.js
+++ src/components/client/ResultsActivities.js
@@ -1,4 +1,5 @@
 import React from "react";
+import convertDate from "../../utils/convertDate";
 
 const ResultsActivities = ({ states }) => {
   return (
@@ -11,20 +12,25 @@ const ResultsActivities = ({ states }) => {
           return (
             <span
               key={key}
-              className="border-gray-100 text-gray-600 py-4 pl-6 pr-3 w-full block hover:bg-gray-100 transition duration-150">
+              className="border-gray-100  text-gray-600 py-4 pl-6 pr-3 w-full block hover:bg-gray-100 transition duration-150">
               <img
                 src="https://avatars0.githubusercontent.com/u/35900628?v=4"
                 alt=""
                 className="rounded-full h-6 shadow-md inline-block mr-2"
               />
-              {item?.estado?.nombre}
+              <span className="font-bold">{item?.estado?.nombre} </span>{" "}
+              actividad agregada por
               <span className="font-bold">
                 {" "}
                 {item?.personal?.id_personal === 0
                   ? "CLIENTE"
                   : item?.personal.nombres}
               </span>
-              <span className="text-gray-500 text-xs"> 24 min ago</span>
+              <span className="flex text-gray-500 text-xs text-right justify-end">
+                {" "}
+                {convertDate(item.creado)}
+              </span>
+              <hr />
             </span>
           );
         })}
