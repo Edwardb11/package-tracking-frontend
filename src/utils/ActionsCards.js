376f7475f5fd23264f2554a27b9cb19bd722c3f9
Edward Brito Diaz <37183622+Edwardb11@users.noreply.github.com>
2022-11-21 10:52:27 -0400

    fix: file cards data

--- src/utils/ActionsCards.js
+++ src/utils/ActionsCards.js
@@ -1,9 +1,15 @@
+// staffCardData
+import addStaff from "../img/addStaff.png";
+import viewStaff from "../img/viewStaff.png";
+import Rol from "../img/rol.png";
+
+// packageCardData
 import manage from "../img/manage.png";
 import Suport from "../img/suport.png";
 import packagReady from "../img/packagReady.png";
 import invoice from "../img/invoice.png";
 
-export const packageCardData = [
+export const packageCard = [
   {
     id: 1,
     img: manage,
@@ -31,5 +37,29 @@ export const packageCardData = [
     link: "/admin/PackageReady",
     name: "Paquetes listos",
     description: "Lista de paquetes entregados.",
+  },
+];
+
+export const staffCard = [
+  {
+    id: 1,
+    img: viewStaff,
+    link: "/admin/listStaff",
+    name: "Personal",
+    description: "Todos los empleados de la empresa",
+  },
+  {
+    id: 2,
+    img: addStaff,
+    link: "/admin/registerStaff",
+    name: "Agregar personal",
+    description: "Formulario para agregar un nuevo empleado    ",
+  },
+  {
+    id: 3,
+    img: Rol,
+    link: "/admin/listStaffOld",
+    name: "Personal inactivo",
+    description: "Personales inactivos ",
   },
 ];
