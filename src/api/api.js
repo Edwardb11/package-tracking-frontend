108be22c16c6aae0b97ea50f70bef872f007d55d
Edward Brito Diaz <37183622+Edwardb11@users.noreply.github.com>
2022-11-22 13:49:26 -0400

    api

--- src/api/api.js
+++ src/api/api.js
@@ -1,2 +1,2 @@
-// export const url = "http://localhost:3000/api/v1"
-export const url = "package-tracking-backend-production.up.railway.app/api/v1";
+// export const url = "http://localhost:5000/api/v1"
+export const url = "http://package-tracking-backend-production.up.railway.app/api/v1";
