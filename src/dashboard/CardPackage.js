deebffa759ec583c8140cc85836d4bad47bbd2ea
Edward Brito Diaz <37183622+Edwardb11@users.noreply.github.com>
2022-11-24 09:47:22 -0400

    fix: error

--- src/dashboard/CardPackage.js
+++ src/dashboard/CardPackage.js
@@ -4,9 +4,9 @@ const CardPackage = ({ title, description }) => {
   return (
     <div className="border-b pl-3 mb-4">
       <p className="text-base font-medium text-gray-800 leading-none">
-        {title}
+        {title || ""}
       </p>
-      <p className="text-sm text-gray-500">{description}</p>
+      <p className="text-sm text-gray-500">{description || ""}</p>
     </div>
   );
 };
